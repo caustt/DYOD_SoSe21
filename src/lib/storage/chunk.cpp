0a5b8807ded158ff6d71a8b96f17e7342e567f6e
Christopher Aust <christopher.aust@student.hpi.uni-potsdam.de>
2021-04-19 19:16:33 +0200

    [step5] implement table
    Co-authored-by: Peter Tsayun <petr.tsayun@student.hpi.de>

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -17,22 +17,23 @@ namespace opossum {
 void Chunk::add_segment(std::shared_ptr<BaseSegment> segment) { _segments.emplace_back(segment); }
 
 void Chunk::append(const std::vector<AllTypeVariant>& values) {
-  DebugAssert(values.size() == _segments.size(), "Invalid row size");
+  DebugAssert(values.size() == _segments.size(),
+              ("Invalid row size" + std::to_string(values.size()) + " " + std::to_string(_segments.size())));
   for (size_t column_id{0}; column_id < _segments.size(); ++column_id) {
-    _segments[column_id]->append(values[column_id]);
+    _segments.at(column_id)->append(values.at(column_id));
   }
 }
 
 std::shared_ptr<BaseSegment> Chunk::get_segment(ColumnID column_id) const {
   DebugAssert(column_id < _segments.size(), "Invalid column id");
-  return _segments[column_id];
+  return _segments.at(column_id);
 }
 
 ColumnCount Chunk::column_count() const { return ColumnCount{(uint16_t)_segments.size()}; }
 
 ChunkOffset Chunk::size() const {
   if (_segments.size() > 0) {
-    return _segments[0]->size();
+    return _segments.at(0)->size();
   } else {
     return 0;
   }

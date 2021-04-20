43f54230edcac3dad5798fe6476c456cde5a8801
Christopher Aust <christopher.aust@student.hpi.uni-potsdam.de>
2021-04-20 10:33:32 +0200

    refactoring
    Co-authored-by: Peter Tsayun <petr.tsayun@student.hpi.de>
    Co-authored-by: Moritz Manner  <moritz.manner@student.hpi.de>

--- src/lib/storage/table.cpp
+++ src/lib/storage/table.cpp
@@ -18,56 +18,49 @@
 namespace opossum {
 
 Table::Table(const ChunkOffset target_chunk_size) {
-  // Implementation goes here
   _target_chunk_size = target_chunk_size;
   _chunks.emplace_back(Chunk());
 }
 
 void Table::add_column(const std::string& name, const std::string& type) {
-  // Implementation goes here
   DebugAssert(_chunks.size() == 1, "Invalid amount of chunks");
-  DebugAssert(_chunks.at(0).size() == 0, "Too many rows");
+  DebugAssert(row_count() == 0, "Too many rows");
 
-  resolve_data_type(type, [&](const auto data_type_t) {
-    using ColumnDataType = typename decltype(data_type_t)::type;
-    const auto value_segment = std::make_shared<ValueSegment<ColumnDataType>>();
-    _chunks.at(0).add_segment(value_segment);
-  });
+  _add_segment(type);
 
   _column_names.emplace_back(name);
   _column_types.emplace_back(type);
 }
 
+void Table::_add_segment(const std::string& type) {
+  resolve_data_type(type, [&](const auto data_type_t) {
+    using ColumnDataType = typename decltype(data_type_t)::type;
+    const auto value_segment = std::make_shared<ValueSegment<ColumnDataType>>();
+    _chunks.back().add_segment(value_segment);
+  });
+}
+
+void Table::_create_new_chunk() {
+  _chunks.emplace_back(Chunk());
+  for (size_t column_id = 0; column_id < _column_names.size(); ++column_id) {
+    auto type = _column_types.at(column_id);
+    _add_segment(type);
+  }
+}
+
 void Table::append(const std::vector<AllTypeVariant>& values) {
-  // Implementation goes here
   DebugAssert(_chunks.size() > 0, "No chunks present");
 
   if (_chunks.back().size() >= _target_chunk_size) {
-    _chunks.emplace_back(Chunk());
-
-    for (size_t column_id = 0; column_id < _column_names.size(); ++column_id) {
-      auto type = _column_types.at(column_id);
-
-      resolve_data_type(type, [&](const auto data_type_t) {
-        using ColumnDataType = typename decltype(data_type_t)::type;
-        const auto value_segment = std::make_shared<ValueSegment<ColumnDataType>>();
-        _chunks.back().add_segment(value_segment);
-      });
-    }
+    _create_new_chunk();
   }
 
   _chunks.back().append(values);
 }
 
-ColumnCount Table::column_count() const {
-  // Implementation goes here
-  DebugAssert(_chunks.size() >= 1, "Invalid amount of chunks");
-
-  return _chunks.at(0).column_count();
-}
+ColumnCount Table::column_count() const { return _chunks.at(0).column_count(); }
 
 uint64_t Table::row_count() const {
-  // Implementation goes here
   uint64_t total_row_count = 0;
 
   for (const auto& chunk : _chunks) {
@@ -77,10 +70,7 @@ uint64_t Table::row_count() const {
   return total_row_count;
 }
 
-ChunkID Table::chunk_count() const {
-  // Implementation goes here
-  return ChunkID{(uint16_t)_chunks.size()};
-}
+ChunkID Table::chunk_count() const { return ChunkID{(uint16_t)_chunks.size()}; }
 
 ColumnID Table::column_id_by_name(const std::string& column_name) const {
   auto result = std::find(_column_names.begin(), _column_names.end(), column_name);
@@ -88,40 +78,21 @@ ColumnID Table::column_id_by_name(const std::string& column_name) const {
   if (result == _column_names.end()) {
     DebugAssert(false, "invalid column name");
   } else {
-    // return ColumnID{(uint32_t) result - _column_names.begin()};
     return ColumnID{(uint16_t)std::distance(_column_names.begin(), result)};
   }
 }
 
-ChunkOffset Table::target_chunk_size() const {
-  // Implementation goes here
-  return _target_chunk_size;
-}
+ChunkOffset Table::target_chunk_size() const { return _target_chunk_size; }
 
-const std::vector<std::string>& Table::column_names() const {
-  // throw std::runtime_error("Implement Table::column_names()");
-  return _column_names;
-}
+const std::vector<std::string>& Table::column_names() const { return _column_names; }
 
-const std::string& Table::column_name(const ColumnID column_id) const {
-  // throw std::runtime_error("Implement Table::column_name");
-  return _column_names.at(column_id);
-}
+const std::string& Table::column_name(const ColumnID column_id) const { return _column_names.at(column_id); }
 
-const std::string& Table::column_type(const ColumnID column_id) const {
-  // throw std::runtime_error("Implement Table::column_type");
-  return _column_types.at(column_id);
-}
+const std::string& Table::column_type(const ColumnID column_id) const { return _column_types.at(column_id); }
 
-Chunk& Table::get_chunk(ChunkID chunk_id) {
-  //  throw std::runtime_error("Implement Table::get_chunk");
-  return _chunks.at(chunk_id);
-}
+Chunk& Table::get_chunk(ChunkID chunk_id) { return _chunks.at(chunk_id); }
 
-const Chunk& Table::get_chunk(ChunkID chunk_id) const {
-  //  throw std::runtime_error("Implement Table::get_chunk");
-  return _chunks.at(chunk_id);
-}
+const Chunk& Table::get_chunk(ChunkID chunk_id) const { return _chunks.at(chunk_id); }
 
 void Table::compress_chunk(ChunkID chunk_id) { throw std::runtime_error("Implement Table::compress_chunk"); }
 

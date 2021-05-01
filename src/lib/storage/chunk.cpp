12b4ffa5a0713b0befe4f2d2cd93d12f75aee12e
Moritz Manner <moritz.manner@gmail.com>
2021-05-01 10:46:49 +0200

    Apply suggestions from code review
    Co-authored-by: vxrahn <33007914+vxrahn@users.noreply.github.com>

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -18,7 +18,7 @@ void Chunk::add_segment(std::shared_ptr<BaseSegment> segment) { _segments.emplac
 
 void Chunk::append(const std::vector<AllTypeVariant>& values) {
   DebugAssert(values.size() == _segments.size(),
-              ("Invalid row size" + std::to_string(values.size()) + " " + std::to_string(_segments.size())));
+              "Invalid size " + std::to_string(values.size()) + " for chunk with size " + std::to_string(_segments.size()));
   for (size_t column_id{0}; column_id < _segments.size(); ++column_id) {
     _segments.at(column_id)->append(values.at(column_id));
   }
@@ -29,11 +29,11 @@ std::shared_ptr<BaseSegment> Chunk::get_segment(ColumnID column_id) const {
   return _segments.at(column_id);
 }
 
-ColumnCount Chunk::column_count() const { return ColumnCount{(uint16_t)_segments.size()}; }
+ColumnCount Chunk::column_count() const { return (ColumnCount) _segments.size(); }
 
 ChunkOffset Chunk::size() const {
-  if (_segments.size() > 0) {
-    return _segments.at(0)->size();
+  if (!_segments.empty()) {
+  return _segments[0]->size();
   } else {
     return 0;
   }

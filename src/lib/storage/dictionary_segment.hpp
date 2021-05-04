7e50326ce991f893c951144f43f2150207c972b6
Moritz Manner <moritz.manner@gmail.com>
2021-05-04 16:07:52 +0200

    Finish implementing fixed-width dictionary segment
    Co-authored-by: Peter Tsayun <petr.tsayun@student.hpi.de>
    Co-authored-by: Christopher Aust  <christopher.aust@student.hpi.uni-potsdam.de>

--- src/lib/storage/dictionary_segment.hpp
+++ src/lib/storage/dictionary_segment.hpp
@@ -57,36 +57,53 @@ class DictionarySegment : public BaseSegment {
   // the DictionarySegment in this file. Replace the method signatures with actual implementations.
 
   // return the value at a certain position. If you want to write efficient operators, back off!
-  AllTypeVariant operator[](const ChunkOffset chunk_offset) const override { return nullptr; };
+  AllTypeVariant operator[](const ChunkOffset chunk_offset) const override { return get(chunk_offset); };
 
   // return the value at a certain position.
-  T get(const size_t chunk_offset) const { return nullptr; }
+  T get(const size_t chunk_offset) const {
+    ValueID value_id = (ValueID)_attribute_vector->at(chunk_offset);
+    return value_by_value_id(value_id);
+  }
 
   // dictionary segments are immutable
-  void append(const AllTypeVariant& val) override {}
+  void append(const AllTypeVariant& val) override {
+    throw std::runtime_error("Append failed: DictionarySegment is immutable.");
+  }
 
   // returns an underlying dictionary
   std::shared_ptr<const std::vector<T>> dictionary() const { return _dictionary; }
 
   // returns an underlying data structure
-  std::shared_ptr<const BaseAttributeVector> attribute_vector() const { return nullptr; }
+  std::shared_ptr<std::vector<u_int32_t>> attribute_vector() const { return _attribute_vector; }
 
   // return the value represented by a given ValueID
-  const T& value_by_value_id(ValueID value_id) const { return nullptr; }
+  const T& value_by_value_id(ValueID value_id) const { return _dictionary->at(value_id); }
 
   // returns the first value ID that refers to a value >= the search value
   // returns INVALID_VALUE_ID if all values are smaller than the search value
-  ValueID lower_bound(T value) const { return ValueID{0}; }
+  ValueID lower_bound(T value) const {
+    auto forward_iterator = std::lower_bound(_dictionary->begin(), _dictionary->end(), value);
+    if (forward_iterator == _dictionary->end()) {
+      return INVALID_VALUE_ID;
+    }
+    return (ValueID)(forward_iterator - _dictionary->begin());
+  }
 
   // same as lower_bound(T), but accepts an AllTypeVariant
-  ValueID lower_bound(const AllTypeVariant& value) const { return ValueID{0}; }
+  ValueID lower_bound(const AllTypeVariant& value) const { return lower_bound(value); }
 
   // returns the first value ID that refers to a value > the search value
   // returns INVALID_VALUE_ID if all values are smaller than or equal to the search value
-  ValueID upper_bound(T value) const { return ValueID{0}; }
+  ValueID upper_bound(T value) const {
+    auto forward_iterator = std::upper_bound(_dictionary->begin(), _dictionary->end(), value);
+    if (forward_iterator == _dictionary->end()) {
+      return INVALID_VALUE_ID;
+    }
+    return (ValueID)(forward_iterator - _dictionary->begin());
+  }
 
   // same as upper_bound(T), but accepts an AllTypeVariant
-  ValueID upper_bound(const AllTypeVariant& value) const { return ValueID{0}; }
+  ValueID upper_bound(const AllTypeVariant& value) const { return upper_bound(value); }
 
   // return the number of unique_values (dictionary entries)
   size_t unique_values_count() const { return _dictionary->size(); }

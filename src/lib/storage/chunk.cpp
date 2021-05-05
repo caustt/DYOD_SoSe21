056528efc193f34a9d5e0092b5c823efdfc285e8
Moritz Manner <moritz.manner@gmail.com>
2021-05-05 16:14:28 +0200

    Encode each segment in its own thread
    Co-authored-by: Peter Tsayun <petr.tsayun@student.hpi.de>
    Co-authored-by: Christopher Aust  <christopher.aust@student.hpi.uni-potsdam.de>

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <mutex>
 #include <string>
+#include <thread>
 #include <utility>
 #include <vector>
 
@@ -19,8 +20,8 @@ namespace opossum {
 void Chunk::add_segment(std::shared_ptr<BaseSegment> segment) { _segments.emplace_back(segment); }
 
 void Chunk::append(const std::vector<AllTypeVariant>& values) {
-  DebugAssert(values.size() == _segments.size(),
-              "Invalid size " + std::to_string(values.size()) + " for chunk with size " + std::to_string(_segments.size()));
+  DebugAssert(values.size() == _segments.size(), "Invalid size " + std::to_string(values.size()) +
+                                                     " for chunk with size " + std::to_string(_segments.size()));
   auto segments_size = _segments.size();
   for (size_t column_id{0}; column_id < segments_size; ++column_id) {
     _segments.at(column_id)->append(values.at(column_id));
@@ -29,31 +30,51 @@ void Chunk::append(const std::vector<AllTypeVariant>& values) {
 
 std::shared_ptr<BaseSegment> Chunk::get_segment(ColumnID column_id) const { return _segments.at(column_id); }
 
-ColumnCount Chunk::column_count() const { return (ColumnCount) _segments.size(); }
+ColumnCount Chunk::column_count() const { return (ColumnCount)_segments.size(); }
 
 ChunkOffset Chunk::size() const {
   if (!_segments.empty()) {
-  return _segments[0]->size();
+    return _segments[0]->size();
   } else {
     return 0;
   }
 }
 
+void Chunk::_encode_segment(std::shared_ptr<BaseSegment> segment, ColumnCount column_id, const std::string& column_type,
+                            std::vector<std::shared_ptr<BaseSegment>>& encoded_segments) {
+  resolve_data_type(column_type, [&](const auto data_type_t) {
+    using ColumnDataType = typename decltype(data_type_t)::type;
+
+    std::shared_ptr<DictionarySegment<ColumnDataType>> encoded_segment =
+        std::make_shared<DictionarySegment<ColumnDataType>>(segment);
+    encoded_segments.at(column_id) = encoded_segment;
+  });
+}
+
 std::shared_ptr<Chunk> Chunk::apply_dictionary_encoding(const Chunk& chunk,
-                                                        const std::vector<std::string>& column_names) {
+                                                        const std::vector<std::string>& column_types) {
   std::shared_ptr<Chunk> encoded_chunk = std::make_shared<Chunk>();
   auto column_count = chunk.column_count();
+
+  std::vector<std::thread> threads;
+  threads.reserve(column_count);
+  std::vector<std::shared_ptr<BaseSegment>> encoded_segments;
+  encoded_segments.resize(column_count);
+
   for (ColumnCount column_id{0}; column_id < column_count; column_id++) {
     std::shared_ptr<BaseSegment> segment = chunk.get_segment(ColumnID{column_id});
-    auto type = column_names.at(column_id);
+    auto column_type = column_types.at(column_id);
+    auto thread =
+        std::thread(Chunk::_encode_segment, segment, column_id, std::move(column_type), std::ref(encoded_segments));
+    threads.emplace_back(std::move(thread));
+  }
 
-    resolve_data_type(type, [&](const auto data_type_t) {
-      using ColumnDataType = typename decltype(data_type_t)::type;
+  for (auto& thread : threads) {
+    thread.join();
+  }
 
-      std::shared_ptr<DictionarySegment<ColumnDataType>> encoded_segment =
-                                                             std::make_shared<DictionarySegment<ColumnDataType>>(segment);
-      encoded_chunk->add_segment(encoded_segment);
-    });
+  for (const auto& encoded_segment : encoded_segments) {
+    encoded_chunk->add_segment(encoded_segment);
   }
 
   return encoded_chunk;
